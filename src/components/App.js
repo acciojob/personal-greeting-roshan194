eaa5569268cd1a707fa024c269a2e6f0c334b691
Roshan <roshan@123>
2025-08-12 16:12:06 +0530

    fix default-value

--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,7 @@ const App = () => {
       <input 
         type="text" 
         placeholder="Enter your name" 
+        defaultValue="" 
         value={name} 
         onChange={handleInputChange} 
       />
